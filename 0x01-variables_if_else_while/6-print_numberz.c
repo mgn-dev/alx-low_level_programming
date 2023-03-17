16e527a87edadcb4ee301ccbfd7a2091786efe0d
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-17 16:47:53 -0700

    did task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase, followed by a new line.
+*
+* Return: null
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
