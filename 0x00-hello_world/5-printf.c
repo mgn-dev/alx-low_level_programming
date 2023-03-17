e389ab858d756aa9deb17d4a1f5b192c9a5c806a
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-16 19:53:19 -0700

    did task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Short prints line using printf
+* Return: null
+*/
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", str);
+	return (0);
+}
