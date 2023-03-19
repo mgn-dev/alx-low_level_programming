48107814de250b061e759cfc7f604bb79151bdd1
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-19 12:14:37 -0700

    did task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 
 /**
-* main - prints combinations of 1 digit numbers, followed by a new line.
+* main - prints combinations of 2 digit numbers, followed by a new line.
 *
 * Return: null
 */
 int main(void)
 {
 	int i;
+	int j;
+	int n = 49;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 57; i++)
 	{
-		putchar(i);
-		if (i < 57)
+		for (j = n; j < 58; j++)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(i);
+			putchar(j);
+			if (i < 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
+		n++;
 	}
 	putchar(10);
 	return (0);
