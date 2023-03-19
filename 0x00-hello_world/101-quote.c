5d4f76b56c8a3891bbe381949ac1f6981dc9ae3e
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-19 08:23:43 -0700

    did task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints a string to the standard error.
+*
+* Return: 1
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful - Dora Korpar,");
+	fprintf(stderr, "2016-10-19\n");
+	return (1);
+}
