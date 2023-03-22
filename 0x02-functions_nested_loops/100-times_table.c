c08b3684a9ae374be4aa6a5a0763f6f79785302b
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-22 05:48:33 -0700

    did task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,58 @@
+extern int _putchar(char c);
+
+/**
+* print_times_table - prints n times table
+* @n: the n to create a times table from
+*
+*/
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int result;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
+				if (result > 99)
+				{
+					if (j > 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result > 9)
+				{
+					if (j > 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					if (j > 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(result + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
-void print_to_98(int n);
+extern int _putchar(char c);
+void print_times_table(int n);
