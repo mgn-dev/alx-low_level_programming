18c7759be688221827881229712708ed1053cb21
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-23 13:30:59 -0700

    did task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+* fizz_buzz - fizz buzz test
+*
+*/
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "Fizz Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+
+/**
+* main - run the fizz buzz test function.
+*
+* Return: null
+*/
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
