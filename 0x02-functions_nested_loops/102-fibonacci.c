c06ba710559d8bb0d504f8771b71286a868bd8ee
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-22 20:02:37 -0700

    did task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+* fib - prints fibonacci up to n.
+* @n: the terms of fibonacci to print.
+*
+*/
+void fib(int n)
+{
+	int i;
+	long int prev;
+	long int prev2;
+	long int curr;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			prev2 = 1;
+			printf("%ld", prev2);
+			if (n > 0)
+				printf(", ");
+		}
+		else if (i == 1)
+		{
+			prev = 2;
+			printf("%ld", prev);
+			if (n > 1)
+				printf(", ");
+		}
+		else
+		{
+			curr = prev + prev2;
+			printf("%ld", curr);
+			prev2 = prev;
+			prev = curr;
+			if (i < n - 1)
+				printf(", ");
+		}
+	}
+}
+
+/**
+* main - run the jack_bauer function
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	fib(50);
+	return (0);
+}
