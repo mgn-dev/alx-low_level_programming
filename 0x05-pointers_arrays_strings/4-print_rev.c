e0b15c62c3b948e23655ce0399e210588f547c05
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-27 12:53:53 -0700

    do task 4 of 0x05

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,35 @@
 #include "main.h"
 
 /**
+* _strlen - returns the length of a string.
+* @s: a string argument.
+*
+* Return: length of string argument.
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
 * print_rev - prints a string, in reverse, followed by a new line.
 * @s: the string to print
 *
 */
 void print_rev(char *s)
 {
-	int len = 0;
-
-	while (*s++)
-		len++;
-	len++;
+	int len = _strlen(s);
 
 	while (len >= 0)
 	{
-		_putchar(*s--);
+		_putchar(s[len]);
 		len--;
 	}
 	_putchar('\n');
