db781f5e3f89cda8de1ee148dc8a21a5abda47bc
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-27 13:53:02 -0700

    do task 5 of 0x05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string.
+* @s: a string argument.
+*
+* Return: length of string argument.
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+* rev_string - prints a string, in reverse.
+* @s: the string to print
+*
+*/
+void rev_string(char *s)
+{
+	int len = _strlen(s);
+	int i;
+	char temp;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
