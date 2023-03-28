21d9eba4f6115881c349a52ddc32983b0c073d19
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-28 09:15:02 -0700

    do task 10 of project 0x05

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,20 +11,24 @@ int _atoi(char *s)
 	int sign_count = 0;
 	int i = 0;
 
-	while (!(s[i] >= '0' && s[i] <= '9'))
+	while (!(s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign_count++;
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
 	{
 		result = result * 10 + (s[i++] - '0');
 	}
 
-	if (sign_count % 2 != 0)
-		sign = -1;
+	if (result != 0)
+	{
+		if (sign_count % 2 != 0)
+			sign = -1;
+		result = sign * result;
+	}
 
-	return (sign * result);
+	return (result);
 }
