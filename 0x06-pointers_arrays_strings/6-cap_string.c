ccef5aff91273d4d53cb4ebdf12ccb33934ca309
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-03-30 09:12:32 +0000

    do task 6 of project 0x06

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+/**
+* *cap_string -  capitalizes all words of a string.
+* @s: string to capitalize.
+*
+* Return: capitalized string.
+*/
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	short caps = 0;
+	char sep[13] = {'\t', ' ', '\n', ',', ';', '.', '!',
+					'?', '"', '(', ')', '{', '}'};
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			for (j = 0; j < 13; j++)
+			{
+				if (s[i - 1] == sep[j])
+					caps = 1;
+			}
+
+			if (caps)
+			{
+				s[i] -= 32;
+				caps = 0;
+			}
+		}
+
+		i++;
+	}
+
+	return (s);
+}
