f1b4ebb50cba179392192e6d81c8ae4e00e78300
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-06 01:24:47 +0000

    did task 0 of 0x08 project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,6 @@
 */
 void _puts_recursion(char *s)
 {
-	_putchar(*s++);
-
 	if (*s == '\0')
 	{
 		_putchar('\n');
@@ -16,6 +14,7 @@ void _puts_recursion(char *s)
 	}
 	else
 	{
+		_putchar(*s++);
 		_puts_recursion(s);
 	}
 }
