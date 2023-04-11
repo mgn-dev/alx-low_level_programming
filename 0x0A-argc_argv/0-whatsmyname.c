f180b033041389311f3afa5c6db30985bc95141a
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-11 02:53:33 +0000

    did task 0 of project 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - check the code.
+* @argc: number of arguments.
+* @argv: argument array.
+*
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > -1)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
