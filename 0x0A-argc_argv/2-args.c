1296a467634b48d2b18b4caa31b8b3d4dbc0aa28
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-11 03:05:31 +0000

    do task 2 of project 0x0A

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - check the code.
+* @argc: number of arguments.
+* @argv: argument array.
+*
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
