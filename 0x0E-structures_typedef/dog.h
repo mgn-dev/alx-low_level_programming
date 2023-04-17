f8df973a8730825fa61fd697dd373401a04a8445
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-17 18:52:15 +0000

    do task 0 of project 0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+* struct dog - a structure that is a dog.
+* @name: name of dog.
+* @age: age of dog.
+* @owner: dog's owner.
+*
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
