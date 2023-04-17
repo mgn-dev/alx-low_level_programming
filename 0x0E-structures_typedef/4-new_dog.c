72f9cbb047cc1fc2f58235e4554148d9fe6ee77b
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-17 21:53:28 +0000

    fix task 4 of project 0x0E

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -70,7 +70,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		else
 			dog->name = _strcpy(name);
 
-		dog->age = age;
+		if (age <= 0)
+			dog->age = 0;
+		else
+			dog->age = age;
 
 		if (owner == NULL)
 			dog->owner = NULL;
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,9 @@
+/**
+* free_dog -
+* @d:
+*
+*/
+void free_dog(dog_t *d);
+{
+	return (0);
+}
