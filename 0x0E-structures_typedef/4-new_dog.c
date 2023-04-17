4e948f0ea6b4619606746244083d46e7b7f090bc
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-17 20:24:10 +0000

    do task 4 of project 0x0E

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "dog.h"
+
+/**
+* new_dog - function that creates a new dog.
+* @name: name of dog.
+* @age: age of dog.
+* @owner: dog's owner.
+*
+* Return: pointer to a dog structure.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		dog->name = name;
+		dog->age = age;
+		dog->owner = owner;
+
+		return (dog);
+	}
+}
