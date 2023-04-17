347ce561d1fa887a24a303e0527db3b727b3e62e
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-17 20:01:28 +0000

    fix task 2 of project 0x0E

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,10 +19,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		if (d->age == 0)
-			printf("Age: %s\n", "(nil)");
-		else
-			printf("Age: %5f\n", d->age);
+		printf("Age: %5f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("Owner: %s\n", "(nil)");
