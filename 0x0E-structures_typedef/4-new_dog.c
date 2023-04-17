8fbc7f9ba593143d5092c6a5ff82431f29575787
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-17 21:36:21 +0000

    fix task 4 of project 0x0E

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -59,7 +59,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 
-	if (dog == NULL)
+	if (dog == NULL || name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
