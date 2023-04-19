588485c154e6165630cd7ba7791ca37986769395
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-19 23:44:24 +0000

    fix task 0 of project 0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #include <stdio.h>
 #include "function_pointers.h"
 
@@ -11,7 +10,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 	{
 		(*f)(name);
 	}
