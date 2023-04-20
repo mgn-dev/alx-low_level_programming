0ed54e6ca14e3387c2e439ce77d10c522ebb0bba
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-20 21:24:30 +0000

    do task 1 of project 0x10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,12 +12,16 @@ void print_numbers(const char *sep, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	if (n == 0)
+		return;
+
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		(i < (n - 1)) ? ((sep) ? printf("%s ", sep) : printf(" ")) : printf("\n");
+		(i < (n - 1)) ? ((sep != NULL) ? printf("%s ", sep) :
+		printf(" ")) : printf("\n");
 	}
 
 	va_end(ap);
