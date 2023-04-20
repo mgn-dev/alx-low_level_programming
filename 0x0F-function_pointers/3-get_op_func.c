a02abd6bfa6ad073d7e32373b7643c65b52c929e
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-20 13:42:20 +0000

    fix task 3 of project 0x0F

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int a, int b)
 	};
 	int i = 0;
 
-	while (s[1] == '\0' && s[0] != ops[i].op[0])
+	while (s[1] == '\0' && s[0] != ops[i].op[0] && s != NULL)
 		i++;
 
 	if (s[1] == '\0' && s[0] == ops[i].op[0] && s != NULL)
