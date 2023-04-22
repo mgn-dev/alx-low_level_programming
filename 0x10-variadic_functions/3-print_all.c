4d87e9090e5ed19b9a008cba4921a49d2b9e7cdb
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-22 02:10:22 +0000

    do task 3 of project 0x10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,23 +13,42 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i, j;
-	char c;
+	unsigned int i = 0, n = 0;
+	char *s;
 
-	s_forms arr[] = {  {'c', (char)va_arg(ap, char)},
-					{'i', (int)va_arg(ap, int)},
-					{'f', (float)va_arg(ap, float)},
-					{'s', (char *)va_arg(ap, char *)}}
+	va_start(ap, format);
+	while (format != NULL && format[n] != '\0')
+		n++;
 
-	va_start(ap, n);
-
-	while (format[i] != '\0')
+	while (i < n)
 	{
-		c = format[i];
-		j = 0;
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			s = va_arg(ap, char *);
+			if (s == NULL)
+			{
+				printf("%s", "(nil)");
+				break;
+			}
+			printf("%s", s);
+			break;
+		}
+
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+			format[i] == 's') && i < n - 1)
+			printf(", ");
 		i++;
 	}
-
 
 	va_end(ap);
 	printf("\n");
