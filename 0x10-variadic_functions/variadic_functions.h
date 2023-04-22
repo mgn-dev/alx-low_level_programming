d35ef6dbd1bc34b3063dd3f43c29c0f66b425268
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-22 02:11:35 +0000

    do task 3 of project 0x10

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,19 +4,6 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *sep, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
-/**
-* struct formats - formats structure
-* @c: character format
-* @f_ptr: The function associated
-*/
-
-typedef struct formats
-{
-    char c;
-    void* (*f_ptr)(int, void*);
-} s_forms;
-
 void print_all(const char * const format, ...);
 
 #endif
