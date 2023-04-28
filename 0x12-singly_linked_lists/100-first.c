6fc469ff9681a7dcb80a9b69cb15983a011db707
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-28 14:00:13 +0000

    do task 5 of project 0x12

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - function that attempts to run before main.
+ *
+*/
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
