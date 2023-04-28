d84e90ad1f0c0b08ab29a59a6dc197a38493596a
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-04-28 09:21:44 +0000

    do task 0 of project 0x12

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list - function that prints all the elements of a list_t list.
+* @h: a linked list node.
+*
+* Return: the number of nodes.
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+		return (0);
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (i);
+}
