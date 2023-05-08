1e7104205a249581b8e52d8ded3762e6bda4ada6
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-05-08 21:21:13 +0000

    fix task 1 of project 0x15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	if (len == 0)
 		return (1);
 
-	if (write(file_des, text_content, len) == -1)
+	if (write(file_des, text_content, len + 1) == -1)
 		return (-1);
 	else
 		return (1);
