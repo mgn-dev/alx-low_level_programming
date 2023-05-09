479e111e1d1ae2dea994557a61dec52f7cc7863f
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-05-09 00:47:20 +0000

    fix task 3 of project 0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -67,7 +67,7 @@ int main(int ac, char **av)
 	fd_r = open(av[1], O_RDONLY);
 	read_failed(fd_r, av[1]);
 
-	fd_w = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	fd_w = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
 	write_failed(fd_w, av[2]);
 
 	do {
