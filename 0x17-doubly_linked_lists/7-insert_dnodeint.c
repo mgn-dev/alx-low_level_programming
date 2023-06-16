2c4cca518d1b1009df3e5ef4651624ea7f5f9f9c
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-16 03:17:30 +0000

    fix task 7 of project 0x17

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
 /**
+* dlistint_len - function that returns the number of elements
+* in a linked dlistint_t list.
+* @h: pointer to the head of dlistint_t list.
+*
+* Return: number of elements.
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int size = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
+
+/**
  * insert_dnodeint_at_index - function that inserts a new node at
  * a given position.
  * @h: pointer to head of dlistint_t list.
@@ -17,22 +37,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL && idx == 0)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-			return (NULL);
-		new_node->n = n;
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		*h = new_node;
+		add_dnodeint(h, n);
 	}
 	else
 	{
 		temp = *h;
 
-		for (i = 0; temp != NULL && i < idx; i++)
+		for (i = 0; temp->next != NULL && i < idx; i++)
 			temp = temp->next;
 
-		if (temp != NULL && i == idx)
+		if (i == idx)
 		{
 			new_node = malloc(sizeof(dlistint_t));
 			if (new_node == NULL)
@@ -46,6 +60,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			while (temp->prev != NULL)
 				temp = temp->prev;
 			 *h = temp;
+		}
+		else if (idx == (unsigned int)dlistint_len(*h))
+		{
+			add_dnodeint_end(h, n);
 		}
 	}
 
