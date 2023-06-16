7bf3325f835c67093ba0617d46efc3ba52cf404b
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-16 02:07:09 +0000

    fix task 7 of project 0x17

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = NULL;
 	unsigned int i;
 
-	if (h == NULL && idx == 0)
+	if (*h == NULL && idx == 0)
 	{
 		new_node = malloc(sizeof(dlistint_t));
 
