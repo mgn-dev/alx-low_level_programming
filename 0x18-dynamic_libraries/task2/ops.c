9af891e02365c9f6f0db8115291d5a3164c4e636
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-20 02:21:03 +0000

    do task 2 of project 0x18

---
+++ 0x18-dynamic_libraries/task2/ops.c
@@ -0,0 +1,61 @@
+#include "ops.h"
+
+/**
+* add - function that adds two integers.
+* @a: first integer
+* @b: second integer
+*
+* Return: addition of two integers.
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - function that subtracts two integers.
+* @a: first integer
+* @b: second integer
+*
+* Return: subtraction of two integers.
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - function that multiplies two integers.
+* @a: first integer
+* @b: second integer
+*
+* Return: multiplication of two integers.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - function that divides two integers.
+* @a: first integer
+* @b: second integer
+*
+* Return: division of two integers
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - function that divides two integers and get remainder.
+* @a: first integer
+* @b: second integer
+*
+* Return: modulus of two integers.
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
