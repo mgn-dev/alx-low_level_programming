19defd5a4cb0ce5a326ce0944e6bdb4255791ddf
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-07 22:12:31 +0000

    Fix task 5, project 0x1A

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,10 +12,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *trv = NULL;
 
 	if (ht == NULL)
-	{
-		printf("{}\n");
 		return;
-	}
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
