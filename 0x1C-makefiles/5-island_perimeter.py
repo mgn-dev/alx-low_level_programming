1e1440dcacc40cbb9e4a868cb209dcd88bf7d5a9
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-11-23 03:46:06 +0200

    Debug task 5 for checker, project 0x1C

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,18 +7,20 @@ def island_perimeter(grid):
     parameter = 0
     nx = len(grid)
 
-    if nx == 0 and type(grid) != type([]):
-        return 0
-
     for x in range(nx):
         ny = len(grid[x])
         for y in range(ny):
+            p = 0
             if ((x - 1 >= 0) and grid[x][y] == 1 and grid[x-1][y] == 0):
-                parameter += 1
+                p += 1
             if ((x + 1 < nx) and grid[x][y] == 1 and grid[x+1][y] == 0):
-                parameter += 1
+                p += 1
             if ((y - 1 >= 0) and grid[x][y] == 1 and grid[x][y-1] == 0):
-                parameter += 1
+                p += 1
             if ((y + 1 < ny) and grid[x][y] == 1 and grid[x][y+1] == 0):
-                parameter += 1
+                p += 1
+
+            if p < 4:
+                parameter += p
+
     return parameter
